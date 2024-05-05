8b8fb78eb6137b586477497d1008716d2c59755d
mohd-ashif <mohdaszif077@gmail.com>
2024-05-05 14:45:54 +0530

    update vercel.json

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const{ db} = require('./db/db')
+const{ db} = require('./db/db.js')
 const ItemModel = require('./model/itemModel.js')
 const invoice = require('./routes/Invoice.js')
 const app = express();
